9746d83ef6ce0d047855eaa062a3bb2428861a69
Patrick <patrickburns2557@gmail.com>
2022-09-05 20:46:35 -0500

    Check for "quit"

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,33 @@ the user and calculate the value of
 a sphere with that radius. 
 **********************************/
 
+use std::io;
+
 const INCH_TO_CENTIMETER: f64 = 2.54;
 const PI: f64 = 3.14159;
 fn main() {
-    let mut input = "";
-
     println!("This program will calculate the volume of a sphere. ");
     println!("You can type \"quit\" at any time to close the program.");
     
     loop {
-        
+        let mut input = String::new();//create variable to store user input
+        println!("Enter the radius of your sphere (in centimeters):");
+
+        //read in input from user, with an error message if it fails
+        io::stdin().read_line(&mut input).expect("Failed to read input.");
+
+        //check if the user inputs "quit" and break out of the loop if so (end program)
+        if input.contains("quit") {
+            break;
+        }
+
+        //convert user input into a f64 variable.
+        //If there's any error in doing so, continues to next iteration of loop
+        let input: f64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
 
 
     }
@@ -33,6 +50,10 @@ fn calculate_volume_cm(input_radius: f64) -> f64 {
     (4.0 / 3.0) * PI * input_radius * input_radius * input_radius
 }
 
+
+//This function calculates the volume of a sphere in cubic inches with the passed in radius
+//Parameters: input_radius - radius of sphere in centimeters
+//Returns: volume of sphere in cubic inches
 fn calculate_volume_in(input_radius: f64) -> f64 {
     let inch_radius = input_radius / INCH_TO_CENTIMETER;
     (4.0 / 3.0) * PI * inch_radius * inch_radius * inch_radius
